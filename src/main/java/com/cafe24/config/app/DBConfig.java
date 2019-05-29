696527c0c84b47a53c4c3beb8c535ca61e7140cd
ydhwa <ydhwa_18@naver.com>
2019-05-29 13:53:00 +0900

    App and Web configuration Java로 설정해봄

---
+++ src/main/java/com/cafe24/config/app/DBConfig.java
@@ -0,0 +1,33 @@
+package com.cafe24.config.app;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DBConfig {
+
+	@Bean
+	public DataSource basicDatasource() {
+		BasicDataSource basicDataSource = new BasicDataSource();
+		basicDataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+		basicDataSource.setUrl("jdbc:mariadb://192.168.1.48:3307/webdb?characterEncoding=utf-8");
+		basicDataSource.setUsername("webdb");
+		basicDataSource.setPassword("webdb");
+		basicDataSource.setInitialSize(10);
+		basicDataSource.setMaxActive(100);
+		
+		return basicDataSource;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+}
