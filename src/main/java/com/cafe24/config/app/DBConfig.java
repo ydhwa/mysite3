4deb68ac00d5a885f46c3209a58454ef1289542e
ydhwa <ydhwa_18@naver.com>
2019-05-29 16:08:40 +0900

    properties (Security 빼고 됨)

--- src/main/java/com/cafe24/config/app/DBConfig.java
+++ src/main/java/com/cafe24/config/app/DBConfig.java
@@ -3,25 +3,32 @@ package com.cafe24.config.app;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:com/cafe24/config/app/properties/jdbc.properties")
 public class DBConfig {
+	
+	@Autowired
+	private Environment env;
 
 	@Bean
 	public DataSource basicDatasource() {
 		BasicDataSource basicDataSource = new BasicDataSource();
-		basicDataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-		basicDataSource.setUrl("jdbc:mariadb://192.168.1.48:3307/webdb?characterEncoding=utf-8");
-		basicDataSource.setUsername("webdb");
-		basicDataSource.setPassword("webdb");
-		basicDataSource.setInitialSize(10);
-		basicDataSource.setMaxActive(100);
+		basicDataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		basicDataSource.setUrl(env.getProperty("jdbc.url"));
+		basicDataSource.setUsername(env.getProperty("jdbc.username"));
+		basicDataSource.setPassword(env.getProperty("jdbc.password"));
+		basicDataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class));
+		basicDataSource.setMaxActive(env.getProperty("jdbc.maxActive", Integer.class));
 		
 		return basicDataSource;
 	}
--- src/main/java/com/cafe24/mysite/config/WebConfig.java
+++ src/main/java/com/cafe24/mysite/config/WebConfig.java
@@ -1,23 +1,21 @@
 package com.cafe24.mysite.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.cafe24.config.web.FileUploadConfig;
+import com.cafe24.config.web.MVCConfig;
+import com.cafe24.config.web.MessageConfig;
+import com.cafe24.config.web.SecurityConfig;
 
 @Configuration
+@EnableAspectJAutoProxy
 @EnableWebMvc
 @ComponentScan({"com.cafe24.mysite.controller"})
+@Import({MVCConfig.class, FileUploadConfig.class, SecurityConfig.class, MessageConfig.class})
 public class WebConfig {
-	
-	@Bean
-	public ViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		resolver.setExposeContextBeansAsAttributes(true);
-		return resolver;
-	}
+
 }
