baa52444fc3b25d97d419c74883b972ac6559645
ydhwa <ydhwa_18@naver.com>
2019-07-09 10:33:29 +0900

    Swagger - II

--- src/main/java/com/cafe24/mysite/controller/api/UserController.java
+++ src/main/java/com/cafe24/mysite/controller/api/UserController.java
@@ -2,11 +2,16 @@ package com.cafe24.mysite.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cafe24.mysite.dto.JSONResult;
 import com.cafe24.mysite.service.UserService;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @RestController("userAPIController")
 @RequestMapping("/user/api")
@@ -15,7 +20,11 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping("/checkemail")
+	@ApiOperation(value="이메일 존재 여부")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name="email", value="이메일 주소", required=true, dataType="string", defaultValue="")
+	})
+	@RequestMapping(value="/checkemail", method=RequestMethod.GET)
 	public JSONResult checkEmail(
 			@RequestParam(value="email", required=true, defaultValue="") String email) {
 		Boolean exist = userService.existEmail(email);
