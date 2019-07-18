5aa1569317f0f2990daa03f99307a9d556678431
ydhwa <ydhwa_18@naver.com>
2019-07-18 11:20:49 +0900

    Spring Security 이용하여 @AuthUser 구현 후, 회원정보 수정 기능 작동시키도록 함

--- src/main/java/com/cafe24/mysite/security/UserDetailsServiceImpl.java
+++ src/main/java/com/cafe24/mysite/security/UserDetailsServiceImpl.java
@@ -1,11 +1,8 @@
 package com.cafe24.mysite.security;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
